9bc1534f7ceac5167df03fa8cea65c8905f82b58
qinzhiyuan9527 <15128488941@139.com>
2020-07-08 20:18:26 +0800

    修改配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,7 +54,6 @@ module.exports = {
                     '/article/Vuex',
                     '/article/tongXin',
                     '/article/VuePress',
-                    '/article/Emoji',
                     '/article/axios'
                 ]
             },
